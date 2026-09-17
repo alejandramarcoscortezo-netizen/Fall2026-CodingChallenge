6d60ab0197af5d70723ec886894032eadc621c1d
alejandramarcoscortezo-netizen <alejandra.marcos.cortezo@gmail.com>
2026-09-17 16:11:15 -0500

    Add collection API endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,39 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+collections = []
 
 @app.route("/")
 def home():
     return jsonify({
         "message": "Image Collection API is running!"
     })
+
+@app.route("/collections", methods=["GET"])
+def get_collections():
+    return jsonify(collections)
+
+@app.route("/collections", methods=["POST"])
+def create_collections():
+    data = request.get_json()
+
+    if not data or not data.get("name"):
+        return jsonify({
+            "error": "Collection name is required"
+        }), 400
+
+    collection = {
+        "id": len(collections) + 1,
+        "name": data["name"],
+        "images": []
+    }
+
+    collections.append(collection)
+
+    return jsonify(collection), 201
 
 
 if __name__ == "__main__":
